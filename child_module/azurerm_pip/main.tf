56a64f0b7067a6ebd81baefbf82c97213c70e98c
aahmad39 <adnanhuntp@gmail.com>
2026-07-28 19:33:42 +0530

    storage account added

--- child_module/azurerm_pip/main.tf
+++ child_module/azurerm_pip/main.tf
@@ -4,5 +4,5 @@ resource "azurerm_public_ip" "pip" {
   resource_group_name = each.value.rg_name
   location            = each.value.location
   allocation_method   = lookup(each.value, "allocation_method", "Static")
-  sku                 = lookup(each.value, "sku", "Standard")
+ 
 }
